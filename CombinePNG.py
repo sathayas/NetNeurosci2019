b5703b9a034422c951dd925c2b7afff82b3ada7f
Satoru Hayasaka <hayasaka@utexas.edu>
2019-05-17 12:40:06 -0500

    Reverted to AVI

--- CombinePNG.py
+++ CombinePNG.py
@@ -9,7 +9,7 @@ images = sorted(images)
 frame = cv2.imread(os.path.join(image_folder, images[0]))
 height, width, layers = frame.shape
 
-video = cv2.VideoWriter(video_name, 0, 5, (width,height))
+video = cv2.VideoWriter(video_name, 0, 10, (width,height))
 
 for image in images:
     video.write(cv2.imread(os.path.join(image_folder, image)))
