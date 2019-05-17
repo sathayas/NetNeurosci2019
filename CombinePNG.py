aa78b595184cf380f0779784bb2cd509a8297f3c
Satoru Hayasaka <hayasaka@utexas.edu>
2019-05-17 12:50:24 -0500

    now mp4 format

--- CombinePNG.py
+++ CombinePNG.py
@@ -2,14 +2,15 @@ import cv2
 import os
 
 image_folder = 'ER_Percolation_PNG'
-video_name = 'ER_Percolation.avi'
+video_name = 'ER_Percolation.mp4'
 
 images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
 images = sorted(images)
 frame = cv2.imread(os.path.join(image_folder, images[0]))
 height, width, layers = frame.shape
 
-video = cv2.VideoWriter(video_name, 0, 10, (width,height))
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+video = cv2.VideoWriter(video_name, fourcc, 10, (width,height))
 
 for image in images:
     video.write(cv2.imread(os.path.join(image_folder, image)))
