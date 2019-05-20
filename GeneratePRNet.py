05b29aa202639403e64ec709bb7bae9416209d29
Satoru Hayasaka <hayasaka@utexas.edu>
2019-05-20 13:36:41 -0500

    All sections are up to date

--- GeneratePRNet.py
+++ GeneratePRNet.py
@@ -4,10 +4,65 @@
 import numpy as np
 import networkx as nx
 import random
+from random import choice
 import matplotlib.pyplot as plt
 
 # seeding the random number generator
 random.seed(2019)
+
+#
+# A function to pick two nodes randomly
+#
+def choose_nodes(g):
+    n1 = choice(list(g.nodes()))
+    n2 = choice(list(g.nodes()))
+    while (n1 == n2):
+        n2 = choice(list(g.nodes()))
+    return n1, n2
+
+#
+# A function to pick two edges randomly
+#
+def choose_edges(g):
+    e1 = choose_nodes(g)
+    while g.has_edge(*e1):   # connectivity check
+        e1 = choose_nodes(g)
+    e2 = choose_nodes(g)
+    while g.has_edge(*e2):   # connectivity check
+        e2 = choose_nodes(g)
+    return e1, e2
+
+#
+# A function to calculate the product of component sizes for an edge
+#
+def prod_comp(g,e):
+    NList1 = nx.node_connected_component(g,e[0])
+    NList2 = nx.node_connected_component(g,e[1])
+    LenC1 = len(NList1)
+    LenC2 = len(NList2)
+    prodC = LenC1 * LenC2
+    return prodC
+
+#
+# A function to add an edge that yields a smaller component size
+#
+def connect_nodes(g):
+    E1, E2 = choose_edges(g)
+    Prod1 = prod_comp(g, E1)
+    Prod2 = prod_comp(g, E2)
+    if Prod1==Prod2:
+        NewE = choice([E1, E2])
+        RejE = choice([E1, E2])
+    else:
+        if Prod1>Prod2:
+            NewE = E2
+            RejE = E1
+        else:
+            NewE = E1
+            RejE = E2
+    g.add_edge(*NewE)
+    return NewE, RejE
+
 
 # Parameters
 N = 200  # Number of nodes
@@ -18,23 +73,17 @@ E = 300  # Number of edges
 G = nx.Graph()
 G.add_nodes_from(list(range(1,N+1)))  # adding nodes
 
-# for loop to add random edges
-edgeList = []
+# initializing the recorders
+edgeList = []   # list of edges added
+rejList = []    # list of rejected edges
+
+# the loop to add random edges
 for iEdge in range(E):
-    # while loop to find an edge that doesnt already exist
-    while True:
-        # picking a random node
-        nodeA = random.randint(1,N)
-        # while loop to pick another node that has not been picked
-        while True:
-            nodeB = random.randint(1,N)
-            if nodeA!=nodeB:
-                break
-        # Checking if there is an edge between nodeA and nodeB
-        if nodeB not in G[nodeA]:
-            G.add_edge(nodeA, nodeB)
-            edgeList.append((nodeA, nodeB))
-            break
+    tmpE, tmpR = connect_nodes(G)
+    edgeList.append(tmpE)
+    rejList.append(tmpR)
+
+
 
 # drawing the graph, just for fun, and to get the coordinates for nodes
 plt.figure(figsize=[5,5])
@@ -46,16 +95,26 @@ plt.axis('off')
 plt.show()
 
 
+
+
 # writing out the edge list, in sequential order
-fNameEdgeList = 'edges_ER_N%d' % N + '_E%d' % E + '.txt'
+fNameEdgeList = 'edges_PR_N%d' % N + '_E%d' % E + '.txt'
 f = open(fNameEdgeList,'w')
 for iEdge in edgeList:
     f.write('%4d  %4d\n' % iEdge)
 f.close()
 
 
+# writing out the rejected edge list, in sequential order
+fNameRejList = 'rejects_PR_N%d' % N + '_E%d' % E + '.txt'
+f = open(fNameEdgeList,'w')
+for iEdge in rejList:
+    f.write('%4d  %4d\n' % iEdge)
+f.close()
+
+
 # writing out the node list, with position
-fNameNodeList = 'nodes_ER_N%d' % N + '_E%d' % E + '.txt'
+fNameNodeList = 'nodes_PR_N%d' % N + '_E%d' % E + '.txt'
 g = open(fNameNodeList,'w')
 nodeList=sorted(pos.items())
 for iNode, iPos in nodeList:
