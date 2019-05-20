7d2a7b06458d8a8e868323cdb4ac311c4e5bc92c
Satoru Hayasaka <hayasaka@utexas.edu>
2019-05-20 12:24:31 -0500

    Now plots GC size

--- PlotGC_ER.py
+++ PlotGC_ER.py
@@ -41,3 +41,32 @@ for iNet in range(nNet):
                 CC = sorted(nx.connected_component_subgraphs(G), key=len, reverse=True)
                 GCSize[iNet,iEdge] = len(CC[0].nodes())
                 break
+
+
+# right panel: giant component size
+plotX = np.arange(1,E+1)
+plotY = np.mean(GCSize,axis=0)/N
+
+plt.figure(figsize=[4,4], facecolor='k')
+plt.subplot(122, position=[0.25,0.15,0.85,0.85])
+plt.plot(plotX[plotX<100], plotY[plotX<100],
+        '-', linewidth=3.0, color='skyblue')
+plt.plot(plotX[plotX>=100], plotY[plotX>=100],
+        '-', linewidth=3.0, color='crimson')
+ax = plt.gca()
+plt.xlim(xLimER)
+plt.ylim(yLimER)
+plt.title('E=%d\n<k>=%4.2f\n' % (len(G.edges()), 2*len(G.edges())/N),
+            color='w', fontsize=18)
+plt.ylabel('Giant component\nsize', color='w', fontsize=18)
+plt.xlabel('<k>', color='w', fontsize=18)
+for axis in ['top','bottom','left','right']:
+  ax.spines[axis].set_linewidth(2)
+  ax.spines[axis].set_color('white')
+ax.set_facecolor('k')
+ax.tick_params(axis='x', colors='white', width=2, which='major', labelsize=14)
+ax.tick_params(axis='x', colors='white', width=2, which='minor')
+ax.tick_params(axis='y', colors='white', width=2, which='major', labelsize=14)
+ax.tick_params(axis='y', colors='white', width=2, which='minor')
+
+plt.show()
