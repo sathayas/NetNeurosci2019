f306059c6d49326bb026395b5f5ff1d75e05e72f
Satoru Hayasaka <hayasaka@utexas.edu>
2019-05-23 15:14:54 -0500

    Bottom left panel (GC vs R) was added

--- ThreshR.py
+++ ThreshR.py
@@ -17,15 +17,18 @@ def net_thR(R, nodes, thR):
     return G
 
 ###### parameters
-posTopLeft = [0.1, 0.55, 0.3, 0.3]
-posTopRight = [0.5, 0.5, 0.372, 0.45]
+posTopLeft = [0.1, 0.5, 0.3, 0.3]
+posTopRight = [0.5, 0.45, 0.4, 0.45]
+posBotLeft = [0.175, 0.1, 0.35, 0.3]
 figLeft = 0.085
 figRight = 0.95
 figTop = 0.9
 figBottom = 0.075
 figW = 0.3
 figH = 0.3
-thR = 0.65
+thR = np.arange(0.7,0.5,-0.05)
+xLimRGC = [0.4, 0.71]
+yLimRGC = [-0.01, 1.01]
 
 ###### Loadin the data saved in .npz file
 f_TS = 'Oxford_sub16112_rt2_K200.npz'
@@ -33,7 +36,7 @@ infile = np.load(f_TS)
 ts = infile['ts']   # time series data: rows: time points, cols: ROIs
 nodes = infile['nodes']   # roi indices
 xyz = infile['xyz']   # roi center cooridates in 3D
-
+N = len(nodes)
 
 # dictionary of xy-coordinates
 pos = {}
@@ -75,62 +78,91 @@ plt.setp(plt.getp(cb.ax.axes, 'yticklabels'), color='w')
 plt.show()
 
 
-
-# Network at this threshold
-G = net_thR(R, nodes, thR)
-
 #
-# Top left pane, thresholded matrix
+# For loop for the threshold
 #
-fig = plt.figure(figsize=[6,6], facecolor='k')
-fig.suptitle('Threshold: R>%5.3f, E=%4d, <k>=%5.3f' % (thR, len(G.edges()), len(G.edges())/len(G.nodes())),
-           color='w', fontsize=18)
+plotR = []
+plotK = []
+plotGC_R = []
+plotGC_K = []
+for iR in thR:
+    # Network at this threshold
+    G = net_thR(R, nodes, iR)
 
-plt.subplot(221, position=posTopLeft)
+    #
+    # Top left pane, thresholded matrix
+    #
+    fig = plt.figure(figsize=[6,6], facecolor='k')
+    fig.suptitle('Threshold: R>%5.3f, E=%4d, <k>=%5.3f' % (iR, len(G.edges()), len(G.edges())/len(G.nodes())),
+               color='w', fontsize=18)
 
-# showing the correlation coefficient
-bR = (R>thR).astype(int)
-plt.imshow(bR, cmap='gray')
-ax = plt.gca()
-plt.title('Thresholded\ncorrelation matrix', color='w', fontsize=12)
-# axis business
-for axis in ['top','bottom','left','right']:
-  ax.spines[axis].set_linewidth(2)
-  ax.spines[axis].set_color('white')
-ax.set_facecolor('k')
-ax.tick_params(axis='x', colors='white', width=1, which='major', labelsize=12)
-ax.tick_params(axis='x', colors='white', width=1, which='minor')
-ax.tick_params(axis='y', colors='white', width=1, which='major', labelsize=12)
-ax.tick_params(axis='y', colors='white', width=1, which='minor')
+    plt.subplot(221, position=posTopLeft)
+
+    # showing the correlation coefficient
+    bR = (R>iR).astype(int)
+    plt.imshow(bR, cmap='gray')
+    ax = plt.gca()
+    plt.title('Thresholded\ncorrelation matrix', color='w', fontsize=12)
+    # axis business
+    for axis in ['top','bottom','left','right']:
+      ax.spines[axis].set_linewidth(2)
+      ax.spines[axis].set_color('white')
+    ax.set_facecolor('k')
+    ax.tick_params(axis='x', colors='white', width=1, which='major', labelsize=12)
+    ax.tick_params(axis='x', colors='white', width=1, which='minor')
+    ax.tick_params(axis='y', colors='white', width=1, which='major', labelsize=12)
+    ax.tick_params(axis='y', colors='white', width=1, which='minor')
 
 
 
-#
-# Top right pane, network
-#
-plt.subplot(222, position=posTopRight)
-nx.draw_networkx_nodes(G, pos, node_size=15, node_color = 'salmon',
-                        linewidth=None)
-nx.draw_networkx_edges(G, pos, edge_color='skyblue', width=1.0)
+    #
+    # Top right pane, network
+    #
+    plt.subplot(222, position=posTopRight)
+    nx.draw_networkx_nodes(G, pos, node_size=15, node_color = 'salmon',
+                            linewidth=None)
+    nx.draw_networkx_edges(G, pos, edge_color='skyblue', width=1.0)
 
-# drawing nodes and edges of the giant component
-CC = sorted(nx.connected_component_subgraphs(G), key=len, reverse=True)
-GC = CC[0]
-nx.draw_networkx_nodes(G, pos, nodelist = GC.nodes(),
-                        node_size=30, node_color = 'orangered',
-                        linewidth=None)
-# drawing all the other components
-if len(CC)>0:
-    for iCC in CC[1:]:
-        if len(iCC)>1:
-            nx.draw_networkx_nodes(G, pos, nodelist = iCC.nodes(),
-                                    node_size=15, node_color = 'crimson',
-                                    linewidth=None)
-plt.axis('off')
-# text for R
-plt.text(38,10,'R', fontsize=12, color='w')
+    # drawing nodes and edges of the giant component
+    CC = sorted(nx.connected_component_subgraphs(G), key=len, reverse=True)
+    GC = CC[0]
+    nx.draw_networkx_nodes(G, pos, nodelist = GC.nodes(),
+                            node_size=30, node_color = 'orangered',
+                            linewidth=None)
+    # drawing all the other components
+    if len(CC)>0:
+        for iCC in CC[1:]:
+            if len(iCC)>1:
+                nx.draw_networkx_nodes(G, pos, nodelist = iCC.nodes(),
+                                        node_size=15, node_color = 'crimson',
+                                        linewidth=None)
+    plt.axis('off')
+    # text for R
+    plt.text(38,10,'R', fontsize=12, color='w')
 
-#plt.subplots_adjust(left=figLeft, right=figRight,
-#                    bottom=figBottom, top=figTop,
-#                    wspace=figW, hspace=figH)
-plt.show()
+
+
+    # right panel: giant component size
+    plotR.append(iR)
+    plotGC_R.append(len(GC.nodes())/N)
+    plt.subplot(223, position=posBotLeft)
+    plt.plot(plotR, plotGC_R,'-', linewidth=3.0, color='skyblue')
+    ax = plt.gca()
+    plt.xlim(xLimRGC)
+    plt.ylim(yLimRGC)
+    plt.ylabel('Giant component\nsize (relative)', color='w', fontsize=14)
+    plt.xlabel('R', color='w', fontsize=14)
+    for axis in ['top','bottom','left','right']:
+      ax.spines[axis].set_linewidth(2)
+      ax.spines[axis].set_color('white')
+    ax.set_facecolor('k')
+    ax.tick_params(axis='x', colors='white', width=2, which='major', labelsize=11)
+    ax.tick_params(axis='x', colors='white', width=2, which='minor')
+    ax.tick_params(axis='y', colors='white', width=2, which='major', labelsize=11)
+    ax.tick_params(axis='y', colors='white', width=2, which='minor')
+
+
+    #plt.subplots_adjust(left=figLeft, right=figRight,
+    #                    bottom=figBottom, top=figTop,
+    #                    wspace=figW, hspace=figH)
+    plt.show()
