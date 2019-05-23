7c9e98bc14999792392b814d2420c995b38f7d10
Satoru Hayasaka <hayasaka@utexas.edu>
2019-05-23 15:41:03 -0500

    placement of panels is fixed

--- ThreshR.py
+++ ThreshR.py
@@ -19,8 +19,8 @@ def net_thR(R, nodes, thR):
 ###### parameters
 posTopLeft = [0.1, 0.5, 0.3, 0.3]
 posTopRight = [0.5, 0.425, 0.4, 0.45]
-posBotLeft = [0.16, 0.1, 0.38, 0.3]
-posBotRight = [0.58, 0.1, 0.38, 0.3]
+posBotLeft = [0.16, 0.08, 0.38, 0.28]
+posBotRight = [0.58, 0.08, 0.38, 0.28]
 figLeft = 0.085
 figRight = 0.95
 figTop = 0.9
@@ -32,6 +32,8 @@ xLimRGC = [0.4, 0.71]
 yLimRGC = [-0.01, 1.01]
 xLimKGC = [0.0, 5.0]
 yLimKGC = [-0.01, 1.01]
+xLimNet = [7.45, 38.25]
+yLimNet = [8.0, 48.25]
 
 
 ###### Loadin the data saved in .npz file
@@ -142,6 +144,8 @@ for iR in thR:
                 nx.draw_networkx_nodes(G, pos, nodelist = iCC.nodes(),
                                         node_size=10, node_color = 'crimson',
                                         linewidth=None)
+    plt.xlim(xLimNet)
+    plt.ylim(yLimNet)
     plt.axis('off')
     # text for R
     plt.title('Resulting network', color='w', fontsize=12)
@@ -194,3 +198,4 @@ for iR in thR:
     #                    bottom=figBottom, top=figTop,
     #                    wspace=figW, hspace=figH)
     plt.show()
+    plt.close()
