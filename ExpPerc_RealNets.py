bf85435a82e1741a4b5a9b7ac3a9bd22902a3448
Satoru Hayasaka <hayasaka@utexas.edu>
2019-05-24 12:27:24 -0500

    Now gene expression network was added

--- ExpPerc_RealNets.py
+++ ExpPerc_RealNets.py
@@ -13,7 +13,8 @@ import matplotlib.pyplot as plt
 
 # parameters
 posLeft = [0.1, 0.15, 0.3, 0.7]
-xLim = [-0.01, 4.3]
+posMid = [0.45, 0.15, 0.3, 0.7]
+xLim = [-0.01, 4.01]
 yLim = [-0.01, 1.01]
 
 
@@ -45,7 +46,7 @@ plt.plot(plotX, rGCSize, '-', color='skyblue', linewidth=2)
 plt.xlim(xLim)
 plt.ylim(yLim)
 ax = plt.gca()
-plt.title('S&P 500 network\n(N=491)', fontsize=14, color='w')
+plt.title('S&P 500 network\n(491 nodes)', fontsize=14, color='w')
 plt.ylabel('Giant component\nsize (relative)', color='w', fontsize=12)
 plt.xlabel('<k>', color='w', fontsize=12)
 for axis in ['top','bottom','left','right']:
@@ -56,6 +57,45 @@ ax.tick_params(axis='x', colors='white', width=1, which='major', labelsize=11)
 ax.tick_params(axis='x', colors='white', width=1, which='minor')
 ax.tick_params(axis='y', colors='white', width=1, which='major', labelsize=11)
 ax.tick_params(axis='y', colors='white', width=1, which='minor')
+
+
+
+#
+# Yeast gene co-expressiond network
+#
+
+# file business
+fRankThNet = 'RankThNet_Yeast.npz'
+
+# loading the file from the previously run analysis
+infile = np.load(fRankThNet)
+nNodes = infile['NNodes']  # Number of nodes
+GCSize = infile['GCSize']  # Giant component size
+nEdges = np.arange(1,len(GCSize)+1)   # Number of edges -- is a series
+
+# calculating variables for plotting
+rGCSize= GCSize / nNodes # relative component size
+plotX = nEdges / nNodes # <k>
+
+# acual plotting
+plt.subplot(132, position=posMid)
+plt.plot(plotX, rGCSize, '-', color='skyblue', linewidth=2)
+plt.xlim(xLim)
+plt.ylim(yLim)
+ax = plt.gca()
+plt.title('Gene co-expression network\n(5168 nodes)', fontsize=14, color='w')
+plt.xlabel('<k>', color='w', fontsize=12)
+for axis in ['top','bottom','left','right']:
+  ax.spines[axis].set_linewidth(1)
+  ax.spines[axis].set_color('white')
+ax.set_facecolor('k')
+ax.tick_params(axis='x', colors='white', width=1, which='major', labelsize=11)
+ax.tick_params(axis='x', colors='white', width=1, which='minor')
+ax.tick_params(axis='y', colors='white', width=1, which='major', labelleft='off')
+ax.tick_params(axis='y', colors='white', width=1, which='minor')
+
+
+
 
 plt.show()
 
